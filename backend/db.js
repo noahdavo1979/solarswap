afeec9bf20635f8a1cbcbbd50ad9002125b04ff7
noahdavo1979 <Noahdavo1979@gmail.com>
2025-10-04 23:36:16 +1000

    Create db.js

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+// backend/db.js
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // needed for Render
+  },
+});
+
+module.exports = pool;
